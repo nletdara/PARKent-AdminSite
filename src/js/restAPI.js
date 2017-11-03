895e034c9cf3e0f166c7ee2f9b9d676245459625
bciavare <bciavare@kent.edu>
2017-11-02 22:01:43 -0400

    yes

--- src/js/restAPI.js
+++ src/js/restAPI.js
@@ -1,18 +1,48 @@
-function getSpots() {
-    return new Promise(resolve => {
-      this.http.get('127.0.0.1:5000/allLots')
-        .map(res => res.json())
-        .subscribe(data => {
-          this.getSpotsResponse = data;
-          document.write(this.getSpotsResponse);
-        });
-    });
-  }
 
 function getAllLots(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
     console.log(theUrl);
+    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.send(null);
+    var response = xmlHttp.responseText;
+    console.log(response);
+    return xmlHttp.responseText;
+}
+
+function addLot(name, cap, lat, lon, url) {
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "http://127.0.0.1:5000/addlot/" + name + "/" + cap + "/" + lat + "/" + lon + "/" + url
+    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.send(null);
+    var response = xmlHttp.responseText;
+    console.log(response);
+    return xmlHttp.responseText;
+}
+
+function dropLot(name) {
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "http://127.0.0.1:5000/removelot/" + name 
+    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.send(null);
+    var response = xmlHttp.responseText;
+    console.log(response);
+    return xmlHttp.responseText;
+}
+
+function createDB(name, lat, lon) {
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "http://127.0.0.1:5000/createdb/" + name + "/" + lat + "/" + lon
+    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.send(null);
+    var response = xmlHttp.responseText;
+    console.log(response);
+    return xmlHttp.responseText;
+}
+
+function dropDB(name) {
+    var xmlHttp = new XMLHttpRequest();
+    var theUrl = "http://127.0.0.1:5000/dropdb/" + name
     xmlHttp.open( "GET", theUrl , false); 
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
