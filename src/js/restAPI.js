b438960186ef7d3900e15b39250deb6bb659581b
Ray Weiss <raymondjohnweiss@gmail.com>
2017-11-07 10:16:27 -0500

    added example of button function

--- src/js/restAPI.js
+++ src/js/restAPI.js
@@ -3,17 +3,22 @@ function getAllLots(theUrl)
 {
     var xmlHttp = new XMLHttpRequest();
     console.log(theUrl);
-    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.open( "GET", theUrl , false);
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
     console.log(response);
     return xmlHttp.responseText;
 }
 
-function addLot(name, cap, lat, lon, url) {
+function addLot(form) {
+    var name = form.location_name.value;
+    var cap =  form.total_spaces.value;
+    var lat =  form.latitude.value;
+    var lon =  form.longitude.value;
+    var url =  form.url.value;
     var xmlHttp = new XMLHttpRequest();
     var theUrl = "http://127.0.0.1:5000/addlot/" + name + "/" + cap + "/" + lat + "/" + lon + "/" + url
-    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.open( "GET", theUrl , false);
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
     console.log(response);
@@ -22,8 +27,8 @@ function addLot(name, cap, lat, lon, url) {
 
 function dropLot(name) {
     var xmlHttp = new XMLHttpRequest();
-    var theUrl = "http://127.0.0.1:5000/removelot/" + name 
-    xmlHttp.open( "GET", theUrl , false); 
+    var theUrl = "http://127.0.0.1:5000/removelot/" + name
+    xmlHttp.open( "GET", theUrl , false);
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
     console.log(response);
@@ -33,7 +38,7 @@ function dropLot(name) {
 function createDB(name, lat, lon) {
     var xmlHttp = new XMLHttpRequest();
     var theUrl = "http://127.0.0.1:5000/createdb/" + name + "/" + lat + "/" + lon
-    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.open( "GET", theUrl , false);
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
     console.log(response);
@@ -43,11 +48,9 @@ function createDB(name, lat, lon) {
 function dropDB(name) {
     var xmlHttp = new XMLHttpRequest();
     var theUrl = "http://127.0.0.1:5000/dropdb/" + name
-    xmlHttp.open( "GET", theUrl , false); 
+    xmlHttp.open( "GET", theUrl , false);
     xmlHttp.send(null);
     var response = xmlHttp.responseText;
     console.log(response);
     return xmlHttp.responseText;
 }
-
-
